c1c29b5c9423e4b5bb2eb3891ff6738f9c6a953c
Mark <afsafa>
2022-09-21 12:13:54 +0800

    Update files

--- data/dataset-template/ImageSets/write_train.py
+++ data/dataset-template/ImageSets/write_train.py
@@ -1,16 +1,13 @@
 import os
-
-label_dir = '../training/label_2'
+file_path = os.path.dirname(os.path.realpath(__file__))
+label_dir = os.path.join(file_path, '../training/label_2')
 
 train = [int(x.split('.')[0]) for x in os.listdir(label_dir)]
 train.sort()
 val = [x for x in train if x %10 ==0]
-# val_len = 33770 - 30001
-# val = [i for i in range(90002, 90002+val_len)]
-# train = [i for i in files if i not in val]
-with open('train.txt', 'w') as out:
+with open(os.path.join(file_path, 'train.txt'), 'w') as out:
     for x in train:
         out.write(f'{x:06}' + "\n")
-with open('val.txt', 'w') as out:
+with open(os.path.join(file_path, 'val.txt'), 'w') as out:
     for x in val:
         out.write(f'{x:06}' + "\n")
