65184b26453006717fd3aa82b41abb99c9eab0db
mark <yahoo55025502@gmail.com>
2023-08-04 11:28:47 +0800

    update configs

--- tools/scripts/revise_json.py
+++ tools/scripts/revise_json.py
@@ -8,9 +8,6 @@ def revise_json(json_path, key, new_value):
     json_content = json5.load(open(json_path, 'r'))
     json_content[key] = new_value
     print(f"Update key {key} to {new_value}")
-    # json_name = os.path.basename(json_path).split('.')[0]
-    # new_json_name = json_name + '_revised' + '.json5'
-    # new_json_path = os.path.join(os.path.dirname(json_path), new_json_name)
     with open(json_path, 'w') as f:
         json.dump(json_content, f, indent=4)
 
